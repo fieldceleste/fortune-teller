cc967bd7db9ac1d49935d13a2220aaeef05ea6e9
Celeste Field <44860174+fieldceleste@users.noreply.github.com>
2020-03-31 16:36:38 -0700

    finish jquery function and add styling to page

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+  $("form#fortune_quiz").submit(function(event) {
+    event.preventDefault();
+    var unlucky_events = [];
+    $("input:checkbox[name=unlucky_events]:checked").each(function() {
+      unlucky_events.push($(this).val());
+    });
+    var lucky_events = [];
+    $("input:checkbox[name=lucky_events]:checked").each(function() {
+      lucky_events.push($(this).val());
+    });
+    if (unlucky_events.length > lucky_events.length)
+      $("#fortune").append("You will have a great rest of the year, and will succeed at anything you put your mind to.")
+    if (unlucky_events.length > lucky_events.length)
+      $("#fortune").append("Something traumatic is about to happen in your life. After this things will get better for you.")
+    if (unlucky_events.length < lucky_events.length)
+      $("#fortune").append("You may not have such a great rest of the year. There may be some obstacles in your way.")
+    if (unlucky_events.length = lucky_events.length)
+      $("#fortune").append("Things are coming togther for you. You may meet someone very important this year.")
+
+    $("#fortune").show();
+    $("#fortune_quiz").hide();
+
+  });
+});
